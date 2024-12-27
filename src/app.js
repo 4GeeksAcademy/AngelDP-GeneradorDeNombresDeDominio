a0775061c37f25ec98ec3eac36264861366c9d98
Angel Ponte <angeld0606@gmail.com>
2024-12-27 16:10:13 +0000

    Ejercicio resuelto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = [
+  "the",
+  "our",
+  "my",
+  "your",
+  "their",
+  "his",
+  "her",
+  "its",
+  "a",
+  "that"
+];
+let adj = [
+  "great",
+  "big",
+  "small",
+  "fast",
+  "cool",
+  "brave",
+  "funny",
+  "smart",
+  "strong",
+  "happy"
+];
+let noun = [
+  "jogger",
+  "racoon",
+  "developer",
+  "artist",
+  "wizard",
+  "dancer",
+  "singer",
+  "writer",
+  "gamer",
+  "builder"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let finalString = "";
+
+  for (let i of pronoun) {
+    for (let j of adj) {
+      for (let k of noun) {
+        finalString += `${i}${j}${k}.com<br>`;
+      }
+    }
+  }
+
+  document.getElementById("result").innerHTML = finalString;
 };
